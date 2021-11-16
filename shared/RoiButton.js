f29f906c04f58488e288db603cece002b3f00f3e
MrGForce <willortizloza@hotmail.com>
2021-11-16 22:36:34 +1100

    Add a composite button with icon

--- shared/RoiButton.js
+++ shared/RoiButton.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { View, TouchableOpacity } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { View, TouchableOpacity, StyleSheet, Text } from "react-native";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 
 const IconBtn = (props) => {
   return (
@@ -12,4 +12,40 @@ const IconBtn = (props) => {
   );
 };
 
-export { IconBtn };
+const ComposedIconBtn = (props) => {
+  return (
+    <TouchableOpacity
+      onPress={props.onPress}
+      style={[styles.compButton, props.style]}
+    >
+      <View style={styles.container}>
+        <MaterialCommunityIcons
+          name={props.iconName}
+          size={props.size}
+          color={props.color}
+        />
+        <View>
+          <Text>{props.label}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+export { IconBtn, ComposedIconBtn };
+
+const styles = StyleSheet.create({
+  compButton: {
+    borderRadius: 7,
+    elevation: 5,
+    backgroundColor: "#ffff",
+    shadowOffset: { width: 2, height: 5 },
+    shadowOpacity: 0.8,
+    shadowRadius: 9,
+  },
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+  },
+});
